5f77f5838b09c3274f034b455fc1734065382132
calm <calm420@163.com>
2019-03-27 15:15:22 +0800

    .

--- src/components/morePage/js/morePage.jsx
+++ src/components/morePage/js/morePage.jsx
@@ -16,6 +16,9 @@ export default class morePage extends React.Component {
             watchName: "",
             visible: false,
             selected: '',
+            guardianData: {},
+            guardians: [],
+            bindType: "",  //bindType==1  是主监护人  2是副监护人   //valid==1是正常  == 2是未通过
         };
     }
 
@@ -58,13 +61,22 @@ export default class morePage extends React.Component {
                             toBind: true,
                         })
                     } else {
+                        result.response[0].guardians.forEach((v, i) => {
+                            if (v.guardian.colUid == this.state.userId) {
+                                this.setState({
+                                    guardianData: v,
+                                }, () => {
+                                    console.log(this.state.guardianData)
+                                })
+                            }
+                        })
                         this.setState({
                             watchData: result.response,
                             childSex: result.response[0].childSex,
                             watchName: result.response[0].watchName,
                             watchId: result.response[0].id,
                             macAddr: result.response[0].macAddress,
-                            studentId: result.response[0].studentId
+                            studentId: result.response[0].studentId,
                         }, () => {
                             this.getWatch2gById(this.state.watchId)
                             this.getWatchId(this.state.macAddr)
@@ -72,7 +84,7 @@ export default class morePage extends React.Component {
                         })
                     }
                 } else {
-                    Toast.fail(result.msg,1,null,false);
+                    Toast.fail(result.msg, 1, null, false);
                 }
             },
             onError: function (error) {
@@ -94,7 +106,7 @@ export default class morePage extends React.Component {
                         watchId: result.response.id
                     })
                 } else {
-                    Toast.fail(result.msg,1,null,false);
+                    Toast.fail(result.msg, 1, null, false);
                 }
             },
             onError: function (error) {
@@ -117,7 +129,7 @@ export default class morePage extends React.Component {
                         watchName: result.response.watchName,
                     })
                 } else {
-                    Toast.fail(result.msg,1,null,false);
+                    Toast.fail(result.msg, 1, null, false);
                 }
             },
             onError: function (error) {
@@ -134,7 +146,7 @@ export default class morePage extends React.Component {
 
             }, onWarn: function (warnMsg) {
                 console.log(warnMsg, "warnMsg")
-                Toast.info(warnMsg,1,null,false)
+                Toast.info(warnMsg, 1, null, false)
             }, onMessage: function (info) {
                 console.log(info, "info")
             }
@@ -194,12 +206,28 @@ export default class morePage extends React.Component {
 
     //选择
     onSelect = (opt) => {
-        console.log(opt.props)
+        this.state.watchData.forEach((v, i) => {
+            if (v.id == opt.props.macId) {
+                this.setState({
+                    guardians: v.guardians
+                }, () => {
+                    this.state.guardians.forEach((v, i) => {
+                        if (v.guardian.colUid == this.state.userId) {
+                            this.setState({
+                                guardianData: v,
+                            }, () => {
+                                console.log(this.state.guardianData, "guardianData")
+                            })
+                        }
+                    })
+                })
+            }
+        })
         this.setState({
             visible: false,
             watchId: opt.props.macId,
             watchName: opt.props.children,
-            macAddr:opt.props.mac
+            macAddr: opt.props.mac
         }, () => {
             this.getWatch2gById(this.state.watchId)
         });
@@ -212,9 +240,9 @@ export default class morePage extends React.Component {
         });
     };
 
-      /**
-     * 删除弹出框
-     */
+    /**
+   * 删除弹出框
+   */
     showAlert = (event) => {
         event.stopPropagation();
         var phoneType = navigator.userAgent;
@@ -230,6 +258,24 @@ export default class morePage extends React.Component {
         ], phone);
     };
 
+    /**
+   * 退出弹出框
+   */
+    showAlertLogout = (event) => {
+        event.stopPropagation();
+        var phoneType = navigator.userAgent;
+        var phone;
+        if (phoneType.indexOf('iPhone') > -1 || phoneType.indexOf('iPad') > -1) {
+            phone = 'ios'
+        } else {
+            phone = 'android'
+        }
+        const alertInstance = alert('您确定退出登录吗?', '', [
+            { text: '取消', onPress: () => console.log('cancel'), style: 'default' },
+            { text: '确定', onPress: () => this.logout() },
+        ], phone);
+    };
+
     //解绑监护人
     unbindGuardian = () => {
         var param = {
@@ -242,7 +288,7 @@ export default class morePage extends React.Component {
         WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
             onResponse: (result) => {
                 if (result.success) {
-                    Toast.info('解绑成功',1,null,false);
+                    Toast.info('解绑成功', 1, null, false);
                     var data = {
                         method: 'unBindSuccess',
                     };
@@ -250,7 +296,7 @@ export default class morePage extends React.Component {
                     });
                     this.getWatch2gsByGuardianUserId(this.state.userId);
                 } else {
-                    Toast.fail(result.msg,1,null,false);
+                    Toast.fail(result.msg, 1, null, false);
                 }
             },
             onError: function (error) {
@@ -270,10 +316,10 @@ export default class morePage extends React.Component {
         WebServiceUtil.requestLittleAntApi(JSON.stringify(param), {
             onResponse: (result) => {
                 if (result.success && result.response) {
-                    Toast.info('解绑成功',1,null,false);
+                    Toast.info('解绑成功', 1, null, false);
                     this.getWatch2gsByGuardianUserId(this.state.userId);
                 } else {
-                    Toast.fail(result.msg,1,null,false);
+                    Toast.fail(result.msg, 1, null, false);
                 }
             },
             onError: function (error) {
@@ -287,7 +333,7 @@ export default class morePage extends React.Component {
         var watchListData = [];
         data.forEach((v) => {
             watchListData.push(
-                (<Item style={{color:'#333'}} macId={v.id} mac={v.macAddress} key={v.id}>{v.watchName}</Item>)
+                (<Item style={{ color: '#333' }} macId={v.id} mac={v.macAddress} key={v.id}>{v.watchName}</Item>)
             );
         });
         this.setState({
@@ -318,16 +364,14 @@ export default class morePage extends React.Component {
         });
     }
 
-
-    onImgError = ()=>{
-        this.setState({
-            imgSrc:"http://www.maaee.com:80/Excoord_For_Education/userPhoto/default_avatar.png"
-        })
-    }
     render () {
+        console.log(this.state.guardianData.valid == 2 && this.state.guardianData.bindType == 2)
+        console.log(this.state.guardianData.valid)
+        console.log(this.state.guardianData.bindType)
+        console.log(this.state.guardianData)
         return (
             <div id="morePage" className='bg_gray'>
-                <div className='watchSelect am-navbar-blue' style={{display:this.state.toBind ? "none":"block"}}>
+                <div className='watchSelect am-navbar-blue' style={{ display: this.state.toBind ? "none" : "block" }}>
                     <Popover mask
                         overlayClassName="fortest"
                         placement="bottomLeft"
@@ -343,7 +387,7 @@ export default class morePage extends React.Component {
                     >
                         <div style={{
                             height: '22px',
-                            lineHeight:'22px',
+                            lineHeight: '22px',
                             padding: '0 15px 15px',
                             marginRight: '-15px',
                             display: 'flex',
@@ -358,54 +402,54 @@ export default class morePage extends React.Component {
                 <div className='personMsg'>
                     <div className="topPadding"></div>
                     <span className='icon_add' onClick={this.toJupmBind}
-                    style={{ display: this.state.toBind ? "none" : "block" }}
+                        style={{ display: this.state.toBind ? "none" : "block" }}
                     >添加手表</span>
                     <div className="clear"></div>
                     <div className="wrap">
-                        <img src={this.state.childSex == "女" ? "http://60.205.86.217/upload9/2019-03-27/11/33ac8e20-5699-4a94-a80c-80adb4f050e3.png":"http://60.205.86.217/upload9/2019-03-27/11/e4119535-3a05-4656-9b9f-47baa348392e.png"} alt="" onError={this.onImgError.bind(this)} />
+                        <img src={this.state.childSex == "女" ? "http://60.205.86.217/upload9/2019-03-27/11/33ac8e20-5699-4a94-a80c-80adb4f050e3.png" : "http://60.205.86.217/upload9/2019-03-27/11/e4119535-3a05-4656-9b9f-47baa348392e.png"} alt="" />
                         <span className='text_hidden'>
                             {
-                                this.state.toBind ? "未绑定" : this.state.watchName
+                                this.state.toBind || (this.state.guardianData.valid == 2 && this.state.guardianData.bindType == 2) ? "未绑定" : this.state.watchName
                             }
                         </span>
                     </div>
                 </div>
                 <div className="grayBorder"></div>
-                <div style={{display:this.state.toBind ? "none":"block"}}  className='am-list-item am-list-item-middle line_public' onClick={this.toPushClock}>
+                <div style={{ display: this.state.toBind || (this.state.guardianData.valid == 2 && this.state.guardianData.bindType == 2) ? "none" : "block" }} className='am-list-item am-list-item-middle line_public' onClick={this.toPushClock}>
                     <div className="am-list-line">
                         <div className="am-list-content">推送闹钟</div>
                         <div className="am-list-arrow am-list-arrow-horizontal"></div>
                     </div>
                 </div>
-                <div style={{display:this.state.toBind ? "none":"block"}} className='am-list-item am-list-item-middle line_public' onClick={this.toFindWatch}>
+                <div style={{ display: this.state.toBind || (this.state.guardianData.valid == 2 && this.state.guardianData.bindType == 2) ? "none" : "block" }} className='am-list-item am-list-item-middle line_public' onClick={this.toFindWatch}>
                     <div className="am-list-line">
                         <div className="am-list-content">找手表</div>
                     </div>
                 </div>
-                <div className="grayBorder" style={{display:this.state.toBind ? "none":"block"}}></div>
-                <div style={{display:this.state.toBind ? "none":"block"}} className='am-list-item am-list-item-middle line_public' onClick={this.pushWeather}>
+                <div className="grayBorder" style={{ display: this.state.toBind || (this.state.guardianData.valid == 2 && this.state.guardianData.bindType == 2) ? "none" : "block" }}></div>
+                <div style={{ display: this.state.toBind || (this.state.guardianData.valid == 2 && this.state.guardianData.bindType == 2) ? "none" : "block" }} className='am-list-item am-list-item-middle line_public' onClick={this.pushWeather}>
                     <div className="am-list-line">
                         <div className="am-list-content">推送天气</div>
                     </div>
                 </div>
-                <div style={{display:this.state.toBind ? "none":"block"}} className='am-list-item am-list-item-middle line_public' onClick={this.pushContacts}>
+                <div style={{ display: this.state.toBind || (this.state.guardianData.valid == 2 && this.state.guardianData.bindType == 2) ? "none" : "block" }} className='am-list-item am-list-item-middle line_public' onClick={this.pushContacts}>
                     <div className="am-list-line">
                         <div className="am-list-content">推送监护人</div>
                     </div>
                 </div>
-                <div className="grayBorder" style={{display:this.state.toBind ? "none":"block"}}></div>
-                <div style={{display:this.state.toBind ? "none":"block"}} style={{display:this.state.toBind ? "none":"flex"}} className='am-list-item am-list-item-middle line_public' onClick={this.showAlert}>
+                <div className="grayBorder" style={{ display: this.state.toBind || (this.state.guardianData.valid == 2 && this.state.guardianData.bindType == 2) ? "none" : "block" }}></div>
+                <div style={{ display: this.state.toBind || (this.state.guardianData.valid == 2 && this.state.guardianData.bindType == 2) ? "none" : "block" }} style={{ display: this.state.toBind ? "none" : "flex" }} className='am-list-item am-list-item-middle line_public' onClick={this.showAlert}>
                     <div className="am-list-line">
                         <div className="am-list-content">解绑</div>
                     </div>
                 </div>
-                <div style={{display:this.state.toBind ? "block":"none"}}  className='am-list-item am-list-item-middle line_public'  onClick={this.toJupmBind}>
+                <div style={{ display: this.state.toBind ? "block" : "none" }} className='am-list-item am-list-item-middle line_public' onClick={this.toJupmBind}>
                     <div className="am-list-line">
                         <div className="am-list-content">添加手表</div>
                         <div className="am-list-arrow am-list-arrow-horizontal"></div>
                     </div>
                 </div>
-                <div className='am-list-item am-list-item-middle line_public' onClick={this.logout}>
+                <div className='am-list-item am-list-item-middle line_public' onClick={this.showAlertLogout}>
                     <div className="am-list-line">
                         <div className="am-list-content">退出登录</div>
                     </div>
