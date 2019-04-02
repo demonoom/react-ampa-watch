32216381f31f7620cb5b9669703604859b418c75
zhaojuan <343524032@qq.com>
2019-04-02 09:55:00 +0800

    -

--- src/components/morePage/js/clock/clockList.jsx
+++ src/components/morePage/js/clock/clockList.jsx
@@ -168,6 +168,7 @@ export default class clockList extends React.Component {
                     <span className="am-navbar-right"></span>
                 </div>
                 <div className="commonLocation-cont overScroll">
+                    <div className="mask transparent"></div>
                     <div className='grayBorder'></div>
                     <div className="publicList_50">
                         {
