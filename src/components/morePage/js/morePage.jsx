4194316b95edf43be8666a89cd9be38657cb81f8
zhaojuan <343524032@qq.com>
2019-04-18 21:14:03 +0800

    -

--- src/components/morePage/js/morePage.jsx
+++ src/components/morePage/js/morePage.jsx
@@ -416,7 +416,7 @@ export default class morePage extends React.Component {
 
     render () {
         return (
-            <div id="morePage" className='bg_gray publicList_50'>
+            <div id="morePage" className='bg_gray publicList_50 androidPage'>
                
                     <div className='watchSelect am-navbar-blue' style={{ display: this.state.toBind ? "none" : "block" }}>
                         <Popover mask
@@ -432,13 +432,7 @@ export default class morePage extends React.Component {
                             onVisibleChange={this.handleVisibleChange}
                             onSelect={this.onSelect}
                         >
-                            <div style={{
-                                height: '44px',
-                                lineHeight: '44px',
-                                padding: '15px 15px 0',
-                                display: 'flex',
-                                alignItems: 'center',
-                            }}
+                            <div className='watchSelectBtn'
                             >
                                 <span className="icon-back"></span>
                                 <span className='text_hidden watchName'>{this.state.watchName}</span>
